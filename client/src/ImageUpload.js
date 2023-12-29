ac87e369023dfeeb1da2325448251574ac3d8c2b
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2023-12-28 19:52:41 -0800

    style updates

--- client/src/ImageUpload.js
+++ client/src/ImageUpload.js
@@ -1,96 +1,75 @@
-// Imports
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Bar } from 'react-chartjs-2';
 import { CategoryScale, LinearScale, Chart, BarElement } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./ImageUpload.css"
+import './ImageUpload.css';
 
-// Registering necessary Chart.js components
 Chart.register(CategoryScale, LinearScale, BarElement, ChartDataLabels);
 
 const ImageUpload = () => {
-
   const [selectedFile, setSelectedFile] = useState(null);
   const [predictions, setPredictions] = useState(null);
   const [uploadWarning, setUploadWarning] = useState(false);
 
-  // Event handler for when the user selects a new file
   const handleFileChange = (e) => {
-    // Reset predictions and set the newly selected file
     setPredictions(null);
     setSelectedFile(e.target.files[0]);
-    // Reset the upload warning when a new file is selected
     setUploadWarning(false);
   };
 
-  // Event handler for form submission to make predictions
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Check if there is a selected file
     if (!selectedFile) {
       setUploadWarning(true);
       return;
     }
 
-    // Create a FormData object and append the selected image file
     const formData = new FormData();
     formData.append('image', selectedFile);
 
     try {
-      // Send a POST request to the prediction endpoint
       const response = await axios.post('http://localhost:5000/predict', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
+        headers: { 'Content-Type': 'multipart/form-data' },
       });
 
-      // Update state with the prediction results
       setPredictions(response.data);
     } catch (error) {
       console.error('Error occurred during prediction:', error);
     }
   };
 
-
   return (
-    <div className='container-fluid d-flex flex-row justify-content-start p-0 main-container'>
-      <div className="description-box">
-      <h3>Flower Image Classification Project Overview</h3>
+    <div className="container-fluid d-flex flex-row justify-content-start  main-container p-0">
+      <div className="description-box p-4">
+        <h3 className="text-primary mb-4">Flower Image Classification Project Overview</h3>
 
-<h6>Project Details:</h6>
-<p>
-    Developed an image classification project using PyTorch to recognize 102 flower species. Achieved through data
-    preparation, VGG11 model selection, classifier design, model training, and checkpoint saving. Implemented inference
-    for new image predictions and performed sanity checks for validation.
-</p>
+        <h6>Project Details:</h6>
+        <p>
+          Developed an image classification project using PyTorch to recognize 102 flower species. Achieved through data preparation, VGG11 model selection, classifier design, model training, and checkpoint saving. Implemented inference for new image predictions and performed sanity checks for validation.
+        </p>
 
-<h6>Technologies Used:</h6>
-<p>
-    Python (v3.6), 
-    PyTorch (v1.0.0), 
-    Matplotlib, 
-    PIL
-</p>
+        <h6>Technologies Used:</h6>
+        <p>
+          Python (v3.6), PyTorch (v1.0.0), Matplotlib, PIL
+        </p>
 
-<h6>Achievements:</h6>
-<ul>
-    <li>Trained a deep learning model for flower classification.</li>
-    <li>Utilized transfer learning with a pre-trained VGG11 model.</li>
-    <li>Demonstrated expertise in data preprocessing and model evaluation.</li>
-    <li>Created an efficient inference pipeline for real-world predictions.</li>
-</ul>
+        <h6>Achievements:</h6>
+        <ul>
+          <li>Trained a deep learning model for flower classification.</li>
+          <li>Utilized transfer learning with a pre-trained VGG11 model.</li>
+          <li>Demonstrated expertise in data preprocessing and model evaluation.</li>
+          <li>Created an efficient inference pipeline for real-world predictions.</li>
+        </ul>
 
-<p>
-    This project showcases end-to-end machine learning capabilities, highlighting technical skills in model development
-    and the potential for real-world deployment.
-</p>
-
+        <p>
+          This project showcases end-to-end machine learning capabilities, highlighting technical skills in model development and the potential for real-world deployment.
+        </p>
       </div>
 
-      <div className="container mt-5 text-center">
+      <div className="container mt-5 text-center prediction-box mb-5">
         <h1 className="mb-4">Flower Type Classifier</h1>
 
         {/* Form for file upload and prediction */}
@@ -100,31 +79,36 @@ const ImageUpload = () => {
             <button type="submit" className="btn btn-primary">Predict</button>
           </form>
         </div>
+          {uploadWarning && <p className="text-danger">Please upload an image first.</p>}
 
         {/* Display selected image and prediction results */}
         <div className="d-flex flex-wrap justify-content-center gap-3">
-          {uploadWarning && <p>Please upload an image first.</p>}
-          {!predictions && (<div style={{ textAlign: "left", maxWidth: "500px" }}> <p>
-            <strong>How It Works:</strong>
-          </p>
-            <ul>
-              <li><strong>Upload Image:</strong> Select an image of a flower you'd like to identify.</li>
-              <li><strong>Predict:</strong> Click the "Predict" button to let the model analyze the image.</li>
-              <li><strong>View Results:</strong> Instantly receive the top 5 predictions with corresponding probabilities.</li>
-            </ul></div>)}
+          {!predictions && (
+            <div className="alert alert-info p-4" style={{ textAlign: "left", maxWidth: "500px", borderRadius: "10px" }}>
+              <p>
+                <strong className="text-primary">How It Works:</strong>
+              </p>
+              <ul>
+                <li><strong>Upload Image:</strong> Select an image of a flower you'd like to identify.</li>
+                <li><strong>Predict:</strong> Click the "Predict" button to let the model analyze the image.</li>
+                <li><strong>View Results:</strong> Instantly receive the top 5 predictions with corresponding probabilities.</li>
+              </ul>
+            </div>
+          )}
+
           {predictions && selectedFile && (
-            <div className="selected-image-container">
+            <div className="selected-image-container rounded">
               <h4>Selected Image:</h4>
               <img
                 src={URL.createObjectURL(selectedFile)}
                 alt="Selected"
-                className="selected-image"
+                className="selected-image img-fluid"
               />
             </div>
           )}
 
           {predictions && (
-            <div>
+            <div className=" rounded">
               <h4>Predictions:</h4>
               {/* Bar chart for displaying prediction probabilities */}
               <Bar
@@ -134,7 +118,7 @@ const ImageUpload = () => {
                     {
                       label: 'Probability',
                       data: predictions.probabilities,
-                      backgroundColor: '#9BD0F5',
+                      backgroundColor: '#66c2a5',  // Greenish color
                       barThickness: 25,
                     },
                   ],
@@ -151,7 +135,6 @@ const ImageUpload = () => {
                       max: 100,
                       ticks: {
                         beginAtZero: true,
-
                       },
                     },
                   },
@@ -180,16 +163,13 @@ const ImageUpload = () => {
               />
               {/* Display a warning if the highest predicted probability is less than 50% */}
               {predictions.probabilities[0] < 50 ? (
-                <p style={{ margin: 0 }}>Warning: This may not be a recognized flower.</p>
+                <p style={{ margin: 0 }} className="text-danger">Warning: This may not be a recognized flower.</p>
               ) : null}
             </div>
           )}
-
         </div>
       </div>
     </div>
-
-
   );
 };
 
